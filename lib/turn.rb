e59d15cd46a989b05406d2df9f8baa9e8d86e5cf
Angie Kassab <anasab12@gmail.com>
2018-05-06 21:31:30 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+#display_board
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end 
+  
+#valid_move
+def valid_move?(board, index)
+  
+   if index.between?(1,9)
+   if !position_taken?(board, index)
+   true
+   end
+   end
